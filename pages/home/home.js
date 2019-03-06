d3f3d84fb8a879ff607ab1309ee48c4649d439c2
cxydxpx <578479132@qq.com>
2019-03-06 17:28:09 +0800

    Initial Commit

---
+++ pages/home/home.js
@@ -0,0 +1,112 @@
+//home.js
+//获取应用实例
+var app = getApp()
+
+const v = getCurrentPages();
+
+Page({
+  data: {
+    list: [{
+      id: 'view',
+      name: '视图容器',
+      open: true,
+      pages: ['view', 'scroll-view', 'swiper']
+    }, {
+      id: 'content',
+      name: '基础内容',
+      open: false,
+      pages: ['text', 'icon', 'progress']
+    }, {
+      id: 'form',
+      name: '表单组件',
+      open: false,
+      pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'radio', 'slider', 'switch', 'textarea']
+    }, {
+      id: 'nav',
+      name: '导航',
+      open: false,
+      pages: ['navigator']
+    }, {
+      id: 'media',
+      name: '媒体组件',
+      open: false,
+      pages: ['image', 'audio', 'video']
+    }, {
+      id: 'map',
+      name: '地图',
+      pages: ['map']
+    }, {
+      id: 'canvas',
+      name: '画布',
+      pages: ['canvas']
+    }]
+  },
+
+  kindToggle: function (e) {
+      console.log('点击了')
+  },
+
+  /**
+   * 下拉刷新
+   */
+  onPullDownRefresh: function() {
+    console.log("下拉的时候调用");
+  },
+
+  /**
+   * 应该是上拉
+   */
+  onReachBottom: function() {
+    console.log("上拉的时候调用");
+  },
+
+  /**
+   * 分享页吧
+   */
+  onShareAppMessage: function() {
+    return {
+      title: "转发的标题"
+    }
+  },
+
+  // 监听页面加载 此方法只加载一次 可以获取query参数
+  onLoad: function() {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+
+
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+})
